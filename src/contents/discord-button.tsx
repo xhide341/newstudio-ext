c8d6fedd974311b4da537a5fe27e298e8a7a190a
xhide341 <shawnehgn10@gmail.com>
2025-05-27 22:35:47 +0800

    feat: inject discord button

---
+++ src/contents/discord-button.tsx
@@ -0,0 +1,72 @@
+import cssText from "data-text:~/contents/discord-button.css"
+import type { PlasmoCSConfig, PlasmoGetInlineAnchor } from "plasmo"
+import type React from "react"
+
+import "./discord-button.css"
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://studio.youtube.com/*", "https://www.youtube.com/*"]
+}
+
+// injectc to shadowdom
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
+}
+
+// style scoping
+export const getShadowHostId = () => "plasmo-discord-button"
+
+export const getInlineAnchor: PlasmoGetInlineAnchor = async () => {
+  const iconsSection = document.querySelector(
+    ".right-section-content .icons-section"
+  )
+  const helpWidget = iconsSection?.querySelector("#help-widget")
+  if (iconsSection && helpWidget) {
+    return {
+      element: helpWidget,
+      insertPosition: "beforebegin"
+    }
+  }
+  console.error("failed to inject discord button")
+  return null
+}
+
+interface DiscordButtonProps {
+  discordUrl?: string
+}
+
+const DiscordButton: React.FC<DiscordButtonProps> = ({
+  discordUrl = "https://discord.newstudio.app/"
+}) => {
+  const handleClick = () => {
+    window.open(discordUrl, "_blank")
+  }
+
+  return (
+    <button
+      id="join-discord-button"
+      className="ns-discord-button"
+      aria-label="Join the Discord"
+      type="button"
+      onClick={handleClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        width="20"
+        height="20">
+        <path
+          d="M83.691 18.816a80.8 80.8 0 0 0-19.968-6.195.31.31 0 0 0-.325.152c-.863 1.532-1.816 3.536-2.484 5.11-7.543-1.13-15.047-1.13-22.434 0-.667-1.61-1.656-3.578-2.523-5.11a.32.32 0 0 0-.324-.152 80.6 80.6 0 0 0-19.969 6.195.3.3 0 0 0-.133.114C2.813 37.934-.676 56.473 1.035 74.777c.008.09.059.176.13.23 8.39 6.165 16.519 9.907 24.5 12.384a.31.31 0 0 0 .343-.114 58 58 0 0 0 5.012-8.152.313.313 0 0 0-.168-.434 53 53 0 0 1-7.653-3.644.315.315 0 0 1-.031-.524 51 51 0 0 0 1.52-1.191.3.3 0 0 1 .316-.043c16.058 7.332 33.441 7.332 49.312 0a.3.3 0 0 1 .32.04 42 42 0 0 0 1.524 1.194.314.314 0 0 1-.027.524 50.6 50.6 0 0 1-7.656 3.64.313.313 0 0 0-.168.438 65 65 0 0 0 5.007 8.148.31.31 0 0 0 .344.118c8.016-2.48 16.149-6.223 24.54-12.387a.3.3 0 0 0 .128-.223c2.043-21.168-3.43-39.55-14.508-55.851a.26.26 0 0 0-.129-.114M33.418 63.63c-4.836 0-8.816-4.438-8.816-9.89 0-5.45 3.906-9.891 8.816-9.891 4.95 0 8.895 4.48 8.816 9.89.004 5.453-3.902 9.89-8.816 9.89m32.605 0c-4.835 0-8.816-4.438-8.816-9.89 0-5.45 3.906-9.891 8.816-9.891 4.95 0 8.895 4.48 8.817 9.89 0 5.453-3.867 9.89-8.817 9.89m0 0"
+          stroke="none"
+          fill-rule="nonzero"
+          fill="white"
+          fill-opacity="1"
+        />
+      </svg>
+      Join the Discord
+    </button>
+  )
+}
+
+export default DiscordButton
